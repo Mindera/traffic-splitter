830e5436ab079c4dd7606c72d5685cbbe4f3c718
tiagoesterisco <tiago@esterisco.com>
2017-03-22 22:09:28 +0000

    Listen to the event only once

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ const Splitter = require('./lib/splitter');
 
 try {
   const splitter = new Splitter(config);
-  splitter.on('application_start', () => {
+  splitter.once('application_start', () => {
     console.log('application_start');
   });
   // start should only be called after adding all events, middlewares and rules
   // start can only be called once in each instance
+  splitter.start();
   splitter.start();
 } catch (e) {
   console.log(`${e.name}: ${e.message}`);
