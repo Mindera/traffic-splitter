4bc9b3fc7536e229dfa91937f2b2b6dfbd93ab8c
duartemendes <duartecostamendes@gmail.com>
2017-03-22 15:47:56 +0000

    Change code structure in order to config and logger be associated with their own splitter instance

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 'use strict'
 
 const fs = require('fs')
-let config = JSON.parse(fs.readFileSync('./stuff/config.json'))
+let config = JSON.parse(fs.readFileSync('./conf/config.json'))
 
-const trafficSplitter = require('./lib/splitter')
-// console.log(trafficSplitter)
+const TrafficSplitter = require('./lib/splitter')
+// console.log(TrafficSplitter)
 
-const splitter = trafficSplitter(config)
+// this is optional since the splitter also does this
+if (!TrafficSplitter.isConfigurationValid(config)) {
+  throw new Error('My configuration is invalid!')
+}
+
+const splitter = new TrafficSplitter(config)
+// console.log(splitter)
+
+splitter.on('application_start', () => {
+  console.log('application_start')
+})
+
+// start should only be called after adding all events, middlewares and rules
+// start can only be called once in each instance
+splitter.start()
 
 // only the next line will be available in production
-module.exports = trafficSplitter
+module.exports = TrafficSplitter
