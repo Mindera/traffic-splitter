db947a806d02932e8ccb6009d8e6e8bb753b11b0
tiagoesterisco <tiago@esterisco.com>
2017-03-23 22:42:05 +0000

    Remove class, add private variables

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
-const config = JSON.parse(fs.readFileSync('./conf/config.json'));
-const Splitter = require('./lib/splitter');
+const fs = require('fs')
+const config = JSON.parse(fs.readFileSync('./conf/config.json'))
+const Splitter = require('./lib/splitter')
 
 try {
-  const splitter = new Splitter(config);
-  splitter.once('application_start', () => {
-    console.log('application_start');
-  });
+  const splitter = new Splitter(config)
+  splitter.eventEmitter.on('application_start', () => {
+    console.log('application_start')
+  })
   // start should only be called after adding all events, middlewares and rules
   // start can only be called once in each instance
-  splitter.start();
-  splitter.start();
+  splitter.start()
+  splitter.start()
 } catch (e) {
-  console.log(`${e.name}: ${e.message}`);
+  console.log(`${e.name}: ${e.message}`)
 }
