7edee0de56a418497acc6354a4cec8f7230506b7
duartemendes <duartecostamendes@gmail.com>
2017-03-23 23:13:33 +0000

    Change to arrow functions, Add export method to validate configuration

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const fs = require('fs')
 const config = JSON.parse(fs.readFileSync('./conf/config.json'))
 const Splitter = require('./lib/splitter')
 
-try {
-  const splitter = new Splitter(config)
-  splitter.eventEmitter.on('application_start', () => {
-    console.log('application_start')
-  })
-  // start should only be called after adding all events, middlewares and rules
-  // start can only be called once in each instance
-  splitter.start()
-  splitter.start()
-} catch (e) {
-  console.log(`${e.name}: ${e.message}`)
+if (!Splitter.isConfigurationValid(config)) {
+  throw new Error('My configuration is invalid!')
 }
+
+const splitter = new Splitter(config)
+splitter.events.on('application_start', () => {
+  console.log('application_start')
+})
+
+// start should only be called after adding all events, middlewares and rules
+// start can only be called once in each instance
+splitter.start()
+splitter.start() // will trigger error message
