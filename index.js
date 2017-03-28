1393167257bb88607f8db072c64286c7d4ec0f12
duartemendes <duartecostamendes@gmail.com>
2017-03-28 12:34:58 +0100

    Handle new middlewares and new rules

--- index.js
+++ index.js
@@ -7,11 +7,25 @@ if (!Splitter.isConfigurationValid(config)) {
 }
 
 const splitter = new Splitter(config)
+const log = splitter.getLogger()
+
+splitter.bootstrap((server, config) => {
+  log.info('bootstrap_application_start')
+})
+
+splitter.use((server, config) => {
+  log.info('request_middleware')
+})
+
+splitter.addRule('myHost', (criteria, req) => {
+  return true
+})
+
 splitter.events.on('application_start', () => {
-  console.log('application_start')
+  log.info('event_application_start')
 })
 
 // start should only be called after adding all events, middlewares and rules
 // start can only be called once in each instance
 splitter.start()
-splitter.start() // will trigger error message
+// splitter.start() // will trigger error message
