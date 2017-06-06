4efd67f84b3add5785f71979c653d3a8e7b50fae
duartemendes <duartecostamendes@gmail.com>
2017-06-06 11:14:31 +0100

    Add chalk dependency. Start splitter from executable.

--- bin/traffic-splitter.js
+++ bin/traffic-splitter.js
@@ -1,20 +1,51 @@
 #!/usr/bin/env node
 
 const program = require('commander')
+const chalk = require('chalk')
+
+const error = chalk.bold.red
+const logErr = (msg) => console.log(error(msg))
 
 program
   .version('1.0.0')
-  .option('-c, --conf', 'provide configuration')
+  .description('For better use of traffic-splitter create an isolated app. Take advantage of custom rules, handling events and adding middlewares.')
+  .option('-c, --conf <file>', 'provide configuration')
 
-program
-  .on('--help', () => {
-    console.log('Help is on its way!')
-  })
-
-program
-  .command('init')
-  .action((cmd, options) => {
-    console.log('Splitter shall start! (soon)');
-  })
+program.on('/help', () => program.help())
 
 program.parse(process.argv)
+
+if (!program.conf) {
+  logErr('Configuration missing.')
+  logErr('Use: traffic-splitter -c yourConfiguration.json.')
+  program.help()
+}
+
+if (!program.conf.endsWith('.json')) {
+  logErr('Configuration file must be a JSON file.')
+  process.exit(1)
+}
+
+const fs = require('fs')
+let config
+try {
+  config = JSON.parse(fs.readFileSync(program.conf))
+} catch (err) {
+  logErr(err)
+  process.exit(1)
+}
+
+const TrafficSplitter = require('..')
+
+if (!TrafficSplitter.isConfigurationValid(config)) {
+  logErr('Configuration invalid. Please check traffic-splitter documentation at http://trafficsplitter.io')
+  process.exit()
+}
+
+try {
+  const splitter = new TrafficSplitter(config)
+  splitter.start()
+} catch (err) {
+  logErr(err)
+  process.exit(1)
+}
